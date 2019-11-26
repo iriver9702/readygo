b535a19cda35883ad2ee35a1c500509cda39bcd3
zhang he <he.zhang@cloudminds.com>
2019-11-26 14:29:17 +0800

    add profile

--- memconsume/memconsume.go
+++ memconsume/memconsume.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
 	"runtime"
+	"runtime/pprof"
 	"sync"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpuprofile")
+var memprofile = flag.String("memprofile", "", "write cpuprofile")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatalf("can't open cpuprofile %v", err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("can't start cpuprofile")
+		}
+		defer pprof.StopCPUProfile()
+	}
 	memConsumed := func() uint64 {
 		runtime.GC()
 		var s runtime.MemStats
@@ -22,7 +41,18 @@ func main() {
 	for i := numGoroutines; i > 0; i-- {
 		go noop()
 	}
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("can't open memprofile")
+		}
+		defer f.Close()
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("can't write memprofile")
+		}
+	}
 	wg.Wait()
 	after := memConsumed()
 	fmt.Printf("%.3fkb", float64(after-before)/numGoroutines/1000)
+
 }
